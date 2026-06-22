2eb6b465991f2139197fdc5a814872e5feff1ea9
tnainsi-hub <tnainsi242@gmail.com>
2026-06-22 19:33:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(__dirname));
 // MONGODB CONNECTION
 mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log('Database connected successfully! ✅'))
